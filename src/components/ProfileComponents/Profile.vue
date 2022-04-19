3086e6fa9f5f8af61920b10f6c2dfeb866379967
ahmato <ahmato7@hotmail.com>
2022-04-19 18:40:28 +0200

    user profile

---
+++ src/components/ProfileComponents/Profile.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="header-box">
+    <div class="container flex">
+      <div class="profile-img">
+        <img alt="Vue logo" class="imgPreview" src="../../assets/logo.png" />
+      </div>
+      <ul class="profileInfo">
+        <li>Name: {{ user.full_name }} </li>
+        <li>Email: {{ user.e_mail }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['user'],
+  name: 'user-Profile'
+};
+</script>
+<style scoped>
+.header-box {
+  width: 100%;
+  border-bottom-style: solid;
+  border-bottom-color: green;
+}
+.profile-img {
+  float: left;
+  border-style: solid;
+  border-radius: 50%;
+  margin: 10px 0 10px 10px;
+}
+.imgPreview {
+  height: 80px;
+  border-radius: 50%;
+}
+.profileInfo {
+  height: 50px;
+  float: right;
+  width: 20rem;
+}
+.flex {
+  display: flex;
+}
+ul {
+  list-style-type: none;
+}
+li {
+  padding-top: 5px;
+}
+</style>
---
+++ src/components/ProfileView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Profile :user="user"  />
+  </div>
+</template>
+<script>
+import Profile from '../components/ProfileComponents/Profile.vue';
+
+export default {
+  name: 'ProfileView',
+  props: ['user'],
+  components: {
+    Profile,
+  },
+};
+</script>
+
