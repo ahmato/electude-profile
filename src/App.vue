3c6384d83cafbed6b6abd36c96fa5f13102581c5
ahmato <ahmato7@hotmail.com>
2022-04-19 18:46:34 +0200

    Navbar is done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
   <div id="app">
-      <ProfileView :user="user"  />
+    <Navbar @targetPage="targetPage" />
+    <ProfileView :user="user" />
   </div>
 </template>
 <script>
 import ProfileView from './components/ProfileView.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
     ProfileView,
-
+    Navbar
   },
   data() {
     return {
+      curretPage: 'profile',
       user: {
-        full_name:'Adam Smith',
+        full_name: 'Adam Smith',
         e_mail: 'Adam@gmail.com',
-        password:'123456',
-      },
+        password: '123456'
+      }
     };
   },
+  methods: {
+    targetPage(targetPage) {
+      this.curretPage = targetPage;
+    }
+  }
 };
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="title-box">
+    <div class="editProfile">
+      <button @click="onProfile">Profile</button>
+      <button @click="onEditeProfile">Edit Profile</button>
+      <h4>{{ pageTitle }}</h4>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'pageTitle',
+  data() {
+    return {
+      pageTitle: 'Profile'
+    };
+  },
+  methods: {
+    onEditeProfile() {
+      this.pageTitle = 'Edit Profile';
+      this.$emit('targetPage', 'editProfile');
+    },
+    onProfile() {
+      this.pageTitle = 'Profile';
+      this.$emit('targetPage', 'profile');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.title-box {
+  background-color: green;
+  margin: 0;
+  padding: 0;
+  color: rgb(255, 255, 255);
+  height: 4rem;
+}
+h4 {
+  margin-left: 20rem;
+  padding: 15px;
+  display: inline;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+.editProfile {
+  float: left;
+}
+button {
+  border: 1px solid #521751;
+  color: white;
+  cursor: pointer;
+  border-radius: 6px;
+  font-size: 0.8rem;
+  margin: 10px;
+  width: 5rem;
+  height: 1.5rem;
+  background: rgba(216, 243, 220, 0.164);
+  border: none;
+  background: none;
+  text-decoration: underline;
+  background-color: green !important;
+  padding: 0 !important;
+}
+</style>
